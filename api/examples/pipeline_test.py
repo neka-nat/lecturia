37381d42031c8f46179ca93d729c6bdf777cd1c7
neka-nat <nekanat.stock@gmail.com>
2025-05-23 01:26:45 +0900

    tune params

--- api/examples/pipeline_test.py
+++ api/examples/pipeline_test.py
@@ -17,12 +17,12 @@ async def main():
             fps=15,
             detail="小学生でも分かるように、内容は元気で明るい感じにしてください。最近のニュースをもとに、お米の価格が上がっている理由を説明してください。",
             extra_slide_rules=[],
-            sprite_name="sprite_ancient_scholar.png",
-            voice_type="senior_male",
+            sprite_name="sprite_cat.png",
+            voice_type="cat",
             web_search=True,
         ),
         #work_dir=Path(f"results/{uuid.uuid4()}"),
-        work_dir=Path("results/8b853164-e56c-4670-b2f8-040e97ef270c"),
+        work_dir=Path("results/9a943ff4-d92d-4245-9efc-7d1bc50cdb84"),
     )
     print(result)
 
