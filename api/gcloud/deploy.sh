371af5d1e31a41c2b5cdee99eec1a35d35012cd8
neka-nat <nekanat.stock@gmail.com>
2025-06-19 21:40:33 +0900

    fix deploy shell

--- api/gcloud/deploy.sh
+++ api/gcloud/deploy.sh
@@ -2,23 +2,26 @@
 set -e
 
 # Dockerイメージのビルドとプッシュ
-gcloud builds submit --tag $REPOSITORY_URL/lecturia-container-registry:latest --project $GOOGLE_CLOUD_PROJECT
+gcloud builds submit --tag $REPOSITORY_URL --project $GOOGLE_CLOUD_PROJECT
 
 # apiのデプロイ
-gcloud run deploy $CLOUD_RUN_API_SERVICE_NAME --image $REPOSITORY_URL/lecturia-container-registry:latest \
+gcloud run deploy $CLOUD_RUN_API_SERVICE_NAME --image $REPOSITORY_URL \
     --region $GOOGLE_CLOUD_LOCATION --project $GOOGLE_CLOUD_PROJECT --timeout=60m \
     --command "fastapi" \
     --args "run,src/lecturia/server.py,--port,8080" \
     --service-account $CLOUD_RUN_API_SERVICE_ACCOUNT --memory=2Gi \
     --update-env-vars LECTURIA_WORKER_URL=$LECTURIA_WORKER_URL \
+    --update-env-vars GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET_NAME=$GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET_NAME \
     --allow-unauthenticated
 
 # Workerのデプロイ
-gcloud run deploy $CLOUD_RUN_WORKER_SERVICE_NAME --image $REPOSITORY_URL/lecturia-container-registry:latest \
+gcloud run deploy $CLOUD_RUN_WORKER_SERVICE_NAME --image $REPOSITORY_URL \
     --region $GOOGLE_CLOUD_LOCATION --project $GOOGLE_CLOUD_PROJECT --timeout=60m \
     --command "fastapi" \
     --args "run,src/lecturia/cloud_pipeline/workflow.py,--port,8080" \
     --service-account $CLOUD_RUN_WORKER_SERVICE_ACCOUNT --memory=2Gi \
     --update-env-vars PROJECT_ID=$GOOGLE_CLOUD_PROJECT \
     --update-env-vars SUBSCRIPTION_ID=$CLOUD_RUN_SUBSCRIPTION_ID \
+    --update-env-vars GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET_NAME=$GOOGLE_CLOUD_STORAGE_PUBLIC_BUCKET_NAME \
+    --set-secrets=ANTHROPIC_API_KEY=ANTHROPIC_API_KEY:latest,GOOGLE_API_KEY=GOOGLE_API_KEY:latest,BRAVE_API_KEY=BRAVE_API_KEY:latest \
     --allow-unauthenticated
--- web/README.md
+++ web/README.md
@@ -20,3 +20,10 @@ pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+## Deploy
+
+```bash
+gcloud run deploy --update-env-vars NEXT_PUBLIC_LECTURIA_API_ORIGIN=<API_URL>
+```
