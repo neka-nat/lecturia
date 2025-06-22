dc4a43a423dbad1c1743d54af92e825a74870d2f
neka-nat <nekanat.stock@gmail.com>
2025-06-22 15:25:31 +0900

    update seeder

--- api/examples/seeder.py
+++ api/examples/seeder.py
@@ -1,13 +1,14 @@
-from lecturia.storage import ls_public_bucket
+from lecturia.storage import ls_public_bucket, is_exists_in_public_bucket
 from lecturia.firestore import TaskStatus
 
 
 lecture_ids = ls_public_bucket("lectures")
 
 for lecture_id in lecture_ids:
+    has_events = is_exists_in_public_bucket(f"lectures/{lecture_id}/events.json")
     task_status = TaskStatus(
         id=lecture_id,
-        status="completed",
+        status="completed" if has_events else "pending",
     )
     task_status.save()
     print(f"lecture_id {lecture_id} is saved")
